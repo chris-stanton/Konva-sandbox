d1c21ab6bc0452bb5c820ab872da87a90d2584b1
Chris Stanton <cstanton0760@yahoo.com>
2020-11-10 20:49:47 -0600

    initial commit

---
+++ client.js
@@ -0,0 +1,39 @@
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+let stage = new Konva.Stage({
+  container: 'container',
+  width: width,
+  height: height,
+});
+
+let layer = new Konva.Layer();
+
+// complex dashed and dotted line
+let placeholderLine = new Konva.Line({
+  points: [5, 70, 140, 23, 250, 60, 300, 20],
+  stroke: 'grey',
+  strokeWidth: 4,
+  lineCap: 'round',
+  lineJoin: 'round',
+  draggable: true,
+});
+
+let draggableLine = new Konva.Line({
+  points: [5, 70, 140, 23, 250, 60, 300, 20],
+  stroke: 'red',
+  strokeWidth: 4,
+  lineCap: 'round',
+  lineJoin: 'round',
+  draggable: true,
+});
+
+layer.add(placeholderLine);
+layer.add(draggableLine);
+stage.add(layer);
+
+console.log(layer);
+
+
+  
